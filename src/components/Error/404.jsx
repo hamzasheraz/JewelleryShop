c52b941c2472c268166d0bd47d117c0e37b5285e
hamzasheraz <hamzasheraz429@gmail.com>
2024-04-19 19:50:56 +0500

    404 page Link fixed

--- src/components/Error/404.jsx
+++ src/components/Error/404.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import logo from '../../images/logo.png'
+import React from "react";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <section className="page-404">
-    <div className="container">
+      <div className="container">
         <div className="row">
-            <div className="col-md-12">
-                <a href="index.html">
-                    <img src={logo} alt="site logo" />
-                </a>
-                <h1>404</h1>
-                <h2>Page Not Found</h2>
-                <a href="index.html" className="btn btn-main"><i className="tf-ion-android-arrow-back"></i> Go Home</a>
-                <p className="copyright-text">© 2018 Themefisher All Rights Reserved</p>
-            </div>
+          <div className="col-md-12">
+            <Link to="/home">
+              <img src={logo} alt="site logo" />
+            </Link>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <Link to="/home" className="btn btn-main">
+              <i className="tf-ion-android-arrow-back"></i> Go Home
+            </Link>
+            <p className="copyright-text">
+              © 2018 Themefisher All Rights Reserved
+            </p>
+          </div>
         </div>
-    </div>
-</section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
