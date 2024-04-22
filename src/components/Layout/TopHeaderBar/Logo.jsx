c702525bf63f553045f5dbd1f21a6d6ecd88111e
hamzasheraz <hamzasheraz429@gmail.com>
2024-04-22 18:14:22 +0500

    Logo Link fixed

--- src/components/Layout/TopHeaderBar/Logo.jsx
+++ src/components/Layout/TopHeaderBar/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
     <div className="col-md-4 col-xs-12 col-sm-4">
       <div className="logo text-center">
-        <a href="index.html">
+        <Link to="/home">
           {/* SVG Logo here */}
           <svg
             width="135px"
@@ -37,7 +38,7 @@ const Logo = () => {
               </g>
             </g>
           </svg>
-        </a>
+        </Link>
       </div>
     </div>
   );
