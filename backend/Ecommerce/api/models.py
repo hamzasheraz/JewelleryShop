cb018a4a79fc392853e6fa4fff48a5cdc7383c16
hamzasheraz <hamzasheraz429@gmail.com>
2024-05-06 20:23:14 +0500

    Models name display at backend  made

--- backend/Ecommerce/api/models.py
+++ backend/Ecommerce/api/models.py
@@ -8,11 +8,16 @@ class ShopProducts(models.Model):
     price=models.IntegerField(null=False,blank=False,default=1)
     sale=models.BooleanField(default=False)
 
+    def __str__(self):
+         return f"{self.Product_name} {self.category}"
+
 class ContactUs(models.Model):
      name=models.CharField(max_length=20)
      email= models.EmailField(max_length=70,blank=True,unique=True)
      subject=models.TextField(null=False,blank=False,max_length=50)
      message=models.TextField(null=False,blank=False,max_length=200)
+     def __str__(self):
+          return f"{self.email} {self.subject}"
 
 
 class Cart(models.Model):
